e401780fc3972a235524554bca8c85e13b63708b
xxdhik <andhikapuja2004@gmail.com>
2025-05-27 00:51:01 +0700

    Politic Category

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,17 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CategoryController;
 
 
 //dashboard
 Route::get('/home', [DashboardController::class, 'index']);
+
+Route::get('/home/politik', function () {
+    return view('politik-category');
+});
+
+Route::get('/home/{category:slug}', [CategoryController::class, 'show'])->name('category.show');
 
 //detail berita
 Route::get('/news/{slug}', [NewsController::class, 'show'])->name('berita.show'); //slug dari judul
